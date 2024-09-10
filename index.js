f487227afe79872657f3fb5050ecae1403b4ce71
Muhammad Ranju <mdranju23@gmail.com>
2024-09-10 20:15:15 +0600

    feat: solve problem number 1 calculateTax

---
+++ index.js
@@ -0,0 +1,12 @@
+function calculateTax(income, expenses) {
+  if (income <= 0 || expenses <= 0) {
+    return "Invalid Input";
+  }
+
+  if (income < expenses) {
+    return "Invalid Input";
+  }
+  const total = income - expenses;
+  const totalIncome = total * 0.2;
+  return totalIncome;
+}
